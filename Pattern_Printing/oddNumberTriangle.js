9992b43c58696b145c7388ddd9bd09bfa6cc0349
Sudhanva Kalghatgi <sudhanvakalghatgi3@gmail.com>
2026-01-01 17:19:18 +0530

    All 3 Methods

--- Pattern_Printing/oddNumberTriangle.js
+++ Pattern_Printing/oddNumberTriangle.js
@@ -1,7 +1,25 @@
+// let n = 5;
+// for (let i = 1; i < n + 1; i++) {
+//     for (let j = 1; j < i + 1; j++) {
+//      process.stdout.write(`${2 * j - 1} `) // 2*(j-1)
+//     }
+//     console.log();
+// }
+    
+// let n = 5;
+// for (let i = 1; i < n + 1; i++) {
+//     for (let j = 1; j <= 2 * i - 1; j+= 2) {
+//         process.stdout.write(`${j} `)
+//     }
+//     console.log();
+// }
+    
 let n = 5;
 for (let i = 1; i < n + 1; i++) {
+    let a = 1;
     for (let j = 1; j < i + 1; j++) {
-     process.stdout.write(`${2 * j - 1} `) // 2*(j-1)
+        process.stdout.write(`${a} `);
+        a += 2;
     }
     console.log();
 }
