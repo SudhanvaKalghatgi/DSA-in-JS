c64ebea472ea551ee20e7ea65d1c52211ea775c2
Sudhanva Kalghatgi <sudhanvakalghatgi3@gmail.com>
2026-01-01 12:46:57 +0530

    first commit

---
+++ Pattern_Printing/starRectangle.js
@@ -0,0 +1,12 @@
+let m = 2;
+let n = 4;
+
+for (let i = 1; i < m+1; i++) { //Rows
+    for (let j = 1; j < n+1; j++) { //Cols
+        process.stdout.write("* "); // console.log -> Prints everything in the nextline this hepls to print things in the same line
+    }
+     console.log();
+    }
+        
+        
+    
