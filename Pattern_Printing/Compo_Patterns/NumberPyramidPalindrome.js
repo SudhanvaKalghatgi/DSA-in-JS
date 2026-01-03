5c11113cb5b84cf28927ba610f7d502d18a23751
Sudhanva Kalghatgi <sudhanvakalghatgi3@gmail.com>
2026-01-03 10:24:37 +0530

    Number Pyraming Palindrome

---
+++ Pattern_Printing/Compo_Patterns/NumberPyramidPalindrome.js
@@ -0,0 +1,18 @@
+let n = 4;
+for (let i = 1; i < n + 1; i++) {
+    for (let j = 1; j <= n - i; j++) {
+        process.stdout.write("  ")
+    }
+     for (let j = 1; j <= i; j++) {
+        process.stdout.write(`${j} `)
+    }  
+    for (let j = i - 1; j > 0 ; j--) {
+        process.stdout.write(`${j} `)
+    } 
+    console.log();
+}
+        
+        
+        
+        
+    
