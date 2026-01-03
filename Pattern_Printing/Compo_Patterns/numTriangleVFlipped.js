24ff360d99fd8bf4c0c079943aa0aac09eb25b17
Sudhanva Kalghatgi <sudhanvakalghatgi3@gmail.com>
2026-01-03 06:12:01 +0530

    Added

--- Pattern_Printing/Compo_Patterns/numTriangleVFlipped.js
+++ Pattern_Printing/Compo_Patterns/numTriangleVFlipped.js
@@ -8,5 +8,13 @@ for (let i = 1; i < n + 1; i++) {
     }
     console.log();
 }
-        
-        
+ ////////////////////////////////////       
+      for (let i = 1; i < n + 1; i++) {
+    for (let j = 1; j <= n - i; j++) {
+       process.stdout.write("  ");
+    }
+    for (let j = 1; j < i + 1; j++) {
+       process.stdout.write(`${i} `);
+    }
+    console.log();
+}  
